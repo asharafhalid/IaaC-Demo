8519e87fafd34b5bd2be266e48a04b31f28b6106
asharafhalid <54720560+asharafhalid@users.noreply.github.com>
2020-08-25 14:48:14 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,22 @@
 resource "aws_instance" "db-server" {
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t2.micro"
+  secuirty_groups =[aws_security_group.dbsec.name]
 
   tags = {
     Name = "HelloWorld-2"
+  }
+}
+
+resource "aws_security_group" "dbsec"{
+  name = " db-security-group"
+  description = "Allow access to our db-server"
+  
+  ingress {
+    description="allow SSH fromm terraform"
+    from_port=22
+    to_prort =22
+    protocol ="tcp"
+    cidr_blocks = [0.0.0.0/0"]
   }
 }
