00112a407ea8d88ec4e2010388bebbd61d1fb5b6
asharafhalid <54720560+asharafhalid@users.noreply.github.com>
2020-08-25 14:53:10 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "dbsec"{
   ingress {
     description="allow SSH fromm terraform"
     from_port=22
-    to_prort =22
+    to_port =22
     protocol ="tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
